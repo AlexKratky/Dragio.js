515a5ee0f0cd9115b4e96f0640ce0ca7c89cdc10
Alex Kratky <info@alexkratky.cz>
2019-07-31 20:41:18 +0200

    Initial commit

--- Dragio.js
+++ Dragio.js
@@ -24,10 +24,19 @@ function Dragio(options) {
     this._callback = options.callback || null;
     this._debug = options.debug || false;
     this._paste = options.paste || true;
+    this._justOverlay = options.justOverlay || false;
     
+    this._totalSize = 0;
+    this._uploaded = 0;
+    this._uploadedOfCurrentFile = 0;
+
     this._init();
 }
 
+
+/**
+ * Opens dialog
+ */
 Dragio.prototype.open = function() {
     document.getElementById("dragio__" + this._ID + "__overlay").style.display = "block";
     let id = this._ID;
@@ -37,6 +46,9 @@ Dragio.prototype.open = function() {
     document.querySelector("body").style.overflow = "hidden";
 }
 
+/**
+ * Closes dialog
+ */
 Dragio.prototype.close = function () {
     document.getElementById("dragio__" + this._ID + "__overlay").style.opacity = "0";
     let id = this._ID;
@@ -46,75 +58,336 @@ Dragio.prototype.close = function () {
     document.querySelector("body").style.overflow = "auto";
 }
 
+/**
+ * Submit the form and start uploading.
+ * @param {(string\|object)} [text="Proccessing ..."] The message that will user see.
+ * @param {(string\|null)} [image=null] The image string from paste event.
+ */
 Dragio.prototype.submit = function (text = "Processing ...", image = null) {
+    if (image === null && this._justOverlay === false) {
+        this._displayFileDialog();
+        return;
+    }
     document.getElementById("dragio__" + this._ID + "__icon").innerHTML = 
 `<svg class="dragio__box__icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="50px" height="50px" viewBox="0 0 50 50" version="1.1" fill="#000000">
   <g id="surface290580">
     <path style=" stroke:none;fill-rule:nonzero;fill:#00bfff;fill-opacity:1;" d="M 25 1.03125 L 13.542969 16.65625 L 20.832031 16.65625 L 20.832031 35.40625 L 29.167969 35.40625 L 29.167969 16.65625 L 36.457031 16.65625 Z M 49.988281 45.824219 L 0 45.824219 L 0 50 L 50 50 Z M 49.988281 45.824219 "/>
   </g>
 </svg>`;
+
+
+    document.getElementById("dragio__" + this._ID + "__hint").innerHTML = `<div class="meter orage" id="dragio__` + this._ID + `__progress" style="width: 300px;">
+    <div style="font-size: 14px;" id="dragio__`+ this._ID + `__progress__percent">0%</div>
+    <span style="width: 0%" id="dragio__`+ this._ID + `__progress__bar"></span>
+</div>`;
+
+
     if(image === null) {
-        document.getElementById("dragio__" + this._ID + "__uploadForm").submit.click();
+        //document.getElementById("dragio__" + this._ID + "__uploadForm").submit.click();
+        var data = new FormData(),
+            request;
+
+        for (let i = 0; i < document.getElementById(`dragio__` + this._ID + `__fileToUpload`).files.length; i++) {
+            data.append('files[]', document.getElementById(`dragio__` + this._ID + `__fileToUpload`).files[i]);
+        }
+        console.log(data.get("files[]"));
+        var request = new XMLHttpRequest();
+        request.addEventListener('load', bind(this, function (e) {
+            console.log(request);
+            if (request.response.error === false) {
+                window.location.href = request.response.redirectTo;
+            } else {
+                this.displayMessage("Failed to upload", true);
+            }
+        }));
+        request.upload.addEventListener('progress', bind(this, function (e) {
+            console.log(e);
+
+            var percent_complete = Math.floor((e.loaded / e.total) * 100);
+            console.log(e.loaded);
+            this._updateProgress(percent_complete, e.total);
+        }));
+        request.responseType = 'json';
+        request.open('post', this._URL);
+        request.send(data); 
     } else {
-        var form = '<form action="' + this._pasteURL + '" method="post" id="dragio__'+this._ID+'__pasteForm">' +
+        /*var form = '<form action="' + this._pasteURL + '" method="post" id="dragio__'+this._ID+'__pasteForm">' +
             '<input type="text" name="image" value="' + image + '" />' +
             '<input type="submit" value="Upload Image" name="submit">' +
             '</form>';
         this._container.insertAdjacentHTML('beforeend', form);
-        document.getElementById("dragio__" + this._ID + "__pasteForm").submit.click();
+        document.getElementById("dragio__" + this._ID + "__pasteForm").submit.click();*/
+        var data = new FormData(),
+            request;
+
+        data.append('image', image);
+
+        var request = new XMLHttpRequest();
+        request.addEventListener('load', bind(this, function (e) {
+            if (request.response.error === false) {
+                window.location.href = request.response.redirectTo;
+            } else {
+                this.displayMessage("Invalid screen", true);
+            }
+        }));
+        request.upload.addEventListener('progress', bind(this, function (e) {
+            console.log(e);
+            var percent_complete = Math.floor((e.loaded / e.total) * 100);
+
+            this._updateProgress(percent_complete, e.total);
+        }));
+        request.responseType = 'json';
+        request.open('post', this._pasteURL);
+        request.send(data); 
         
     }
     if(typeof text === "object") {
         document.getElementById("dragio__" + this._ID + "__info").innerHTML = "Processing ...";
     } else {
         document.getElementById("dragio__" + this._ID + "__info").innerHTML = text;
+    
     }
-    document.getElementById("dragio__" + this._ID + "__hint").innerHTML = " ";
+    
 }
 
+/**
+ * Displays custom file dialog with more info. This function is called from submit() function when options.justOverlay is equal to false.
+ */
+Dragio.prototype._displayFileDialog = function () {
+    document.getElementById(`dragio__` + this._ID + `__files`).style.display = "block";
+    var totalSize = 0;
+    for (let i = 0; i < document.getElementById(`dragio__` + this._ID + `__fileToUpload`).files.length; i++) {
+        console.log(document.getElementById(`dragio__` + this._ID + `__fileToUpload`).files[i]);
+        let f = document.getElementById(`dragio__` + this._ID + `__fileToUpload`).files[i];
+        let html = `<tr role="row" style="height: 100px">
+    <td role="cell">
+        <img class="dragio__file-icon" src="" id="dragio__`+ this._ID + `__icon__file` + i +`">
+    </td>
+    <td role="cell">
+        <div class="dragio__file-name" id="dragio__`+ this._ID + `__name__file` + i +`">
+            `+f.name+`
+        </div>
+    </td>
+    <td role="cell">
+        <div class="dragio__file-size">
+            `+this._convertBytes(f.size)+`
+        </div>
+    </td>
+    <td role="cell" style="min-width: 33%;">
+        <div class="dragio__file-progress">
+            <div class="meter">
+                <div style="font-size: 14px; text-align: center; color: white;" id="dragio__`+ this._ID + `__progress__percent__file`+i+`">0%</div>
+                <span style="width: 0%; background: #ff00f4;" id="dragio__`+ this._ID + `__progress__bar__file` + i +`"></span>
+            </div>
+        </div>
+    </td>
+</tr>`;
+        totalSize += f.size;
+        this._totalSize = totalSize;
+        document.getElementById(`dragio__` + this._ID + `__file__dialog`).insertAdjacentHTML('beforeend', html);
+    }
+    let stat = `<tr role="row" style="height: 100px">
+    <td role="cell">
+    </td>
+    <td role="cell">
+        <div class="dragio__file-name">
+        </div>
+    </td>
+    <td role="cell">
+        <div class="dragio__file-size">
+            `+ this._convertBytes(totalSize) + `
+        </div>
+    </td>
+    <td role="cell" style="min-width: 33%;">
+        <div class="dragio__file-progress">
+            <div class="meter">
+                <div style="font-size: 14px; text-align: center; color: white;" id="dragio__`+ this._ID + `__progress__percent__files">0%</div>
+                <span style="width: 0%; background: #ff00f4;" id="dragio__`+ this._ID + `__progress__bar__files"></span>
+            </div>
+        </div>
+    </td>
+</tr>`;
+    document.getElementById(`dragio__` + this._ID + `__file__dialog`).insertAdjacentHTML('beforeend', stat);
+
+    this._loadIcons(0);
+    this._startUploading(0);
+    document.getElementById(`dragio__` + this._ID + `__exit`).style.display = "none";
+    document.getElementById(`dragio__` + this._ID + `__container`).style.display = "none";
+    document.getElementById(`dragio__` + this._ID + `__files`).style.opacity = "1";
+}
+
+/**
+ * Loads icons (image) of selected files. Called from _displayFileDialog()
+ * @param {number} [index=0] The starting index. Should be 0.
+ */
+Dragio.prototype._loadIcons = function(index=0) {
+    if (index >= document.getElementById(`dragio__` + this._ID + `__fileToUpload`).files.length) {
+        return;
+    }
+    var preview = document.getElementById(`dragio__` + this._ID + `__icon__file` + index);
+    var reader = new FileReader();
+
+    reader.addEventListener("load", bind(this, function () {
+        preview.src = reader.result;
+        console.log(preview);
+        this._loadIcons(index+1); 
+    }), false);
+
+    if (document.getElementById(`dragio__` + this._ID + `__fileToUpload`).files[index]) {
+        if (document.getElementById(`dragio__` + this._ID + `__fileToUpload`).files[index].type == "image/jpeg" || document.getElementById(`dragio__` + this._ID + `__fileToUpload`).files[index].type == "image/png") {
+            reader.readAsDataURL(document.getElementById(`dragio__` + this._ID + `__fileToUpload`).files[index]);
+        } else {
+            this._loadIcons(index + 1); 
+        }
+    }
+}
+
+/**
+ * Starts uploading of each file. Called from _displayFileDialog()
+ * @param {number} [index=0] The starting index. Should be 0.
+ */
+Dragio.prototype._startUploading = function(index = 0) {
+    if (index >= document.getElementById(`dragio__` + this._ID + `__fileToUpload`).files.length) {
+        return;
+    }
+    this._uploadedOfCurrentFile = 0;
+    var data = new FormData(),
+        request;
+
+    data.append('files[]', document.getElementById(`dragio__` + this._ID + `__fileToUpload`).files[index]);
+
+    var request = new XMLHttpRequest();
+    request.addEventListener('load', bind(this, function (e) {
+        if (request.response.error === false) {
+            document.getElementById(`dragio__` + this._ID + `__name__file` + index).style.color = "#00ff00";
+        } else {
+            document.getElementById(`dragio__` + this._ID + `__name__file` + index).style.color = "#ff0000";
+        }
+        if (index+1 >= document.getElementById(`dragio__` + this._ID + `__fileToUpload`).files.length) {
+            setTimeout(() => {
+                console.log("COMPLETED");
+                window.location.href = request.response.redirectTo;                
+            }, 1000);
+        } else {
+            this._startUploading(index + 1);
+        }
+    }));
+    request.upload.addEventListener('progress', bind(this, function (e) {
+        var percent_complete = Math.floor((e.loaded / e.total) * 100);
+        this._updateProgressFile(percent_complete, index, e.loaded);
+    }));
+    request.responseType = 'json';
+    request.open('post', this._URL);
+    request.send(data); 
+}
+
+/**
+ * Updates progress bar. Called from submit().
+ * @param {number} val The progress in percent.
+ * @param {number} total The file size.
+ */
+Dragio.prototype._updateProgress = function(val, total) {
+    document.getElementById(`dragio__` + this._ID + `__progress__percent`).innerHTML = val + "% (" + this._convertBytes(total) + ")";
+    document.getElementById(`dragio__` + this._ID + `__progress__bar`).style.width = val + "%";
+}
+
+/**
+ * Updates progress bars in custom file dialog. Called from _displayFileDialog().
+ * @param {number} val The progress in percent.
+ * @param {number} index The file index.
+ * @param {number} bytesUploaded Total size of uploaded bytes of current uploading file.
+ */
+Dragio.prototype._updateProgressFile = function (val, index, bytesUploaded) {
+    document.getElementById(`dragio__` + this._ID + `__progress__percent__file` + index).innerHTML = val + "%";
+    document.getElementById(`dragio__` + this._ID + `__progress__bar__file` + index).style.width = val + "%";
+    this._uploaded += (bytesUploaded - this._uploadedOfCurrentFile);
+    this._uploadedOfCurrentFile = bytesUploaded;
+
+    var percent_complete = Math.floor((this._uploaded / this._totalSize) * 100);
+    document.getElementById(`dragio__` + this._ID + `__progress__percent__files`).innerHTML = percent_complete + "%";
+    document.getElementById(`dragio__` + this._ID + `__progress__bar__files`).style.width = percent_complete + "%";
+
+}
+
+/**
+ * DragEnter Event handler. Change text to "Drop items here" & opens the dialog.
+ * @param {object} e The event.
+ */
 Dragio.prototype._dragEnter = function(e) {
     this._log("dragEnter():");
     this._log(e);
     this.open();
+    document.getElementById("dragio__" + this._ID + "__info").innerHTML = "Drop items here";
+    document.getElementById("dragio__" + this._ID + "__hint").innerHTML = " ";
 }
 
+/**
+ * DragExit Event handler.
+ * @param {object} e The event.
+ */
 Dragio.prototype._dragExit = function (e) {
     this._log("dragExit():");
     this._log(e);
     e.preventDefault();
 }
 
+/**
+ * DragEnd Event handler.
+ * @param {object} e The event.
+ */
 Dragio.prototype._dragEnd = function (e) {
     this._log("dragEnd():");
     this._log(e);
     e.preventDefault();
 }
 
+/**
+ * DragLeave Event handler. Closes the dialog, if the drag leaves the browser.
+ * @param {object} e The event.
+ */
 Dragio.prototype._dragLeave = function (e) {
     this._log("dragLeave():");
     this._log(e);
     e.preventDefault();
     if (!e.clientX && !e.clientY) {
         this.close();
+        document.getElementById("dragio__" + this._ID + "__info").innerHTML = `Drag 'n' drop files here or <span class="dragio__click" id="dragio__` + this._ID + `__click">click here</span>.`;
+        document.getElementById("dragio__" + this._ID + "__hint").innerHTML = "Or press Ctrl + V to upload image from clipboard.";
     }
 }
 
+/**
+ * DragOver Event handler.
+ * @param {object} e The event.
+ */
 Dragio.prototype._dragOver = function (e) {
     //this._log("dragOver():", e);
     e.preventDefault();
 }
 
+/**
+ * DragStart Event handler.
+ * @param {object} e The event.
+ */
 Dragio.prototype._dragStart = function (e) {
     this._log("dragStart():");
     this._log(e);
 }
 
+/**
+ * Drag Event handler.
+ * @param {object} e The event.
+ */
 Dragio.prototype._drag = function (e) {
     this._log("drag():");
     this._log(e);
     e.preventDefault();
 }
 
+/**
+ * Drop Event handler. Submit the form or call the custom options.callback
+ * @param {object} e The event.
+ */
 Dragio.prototype._drop = function (e) {
     e.preventDefault();
     this._log("drop():");
@@ -128,10 +401,19 @@ Dragio.prototype._drop = function (e) {
     }
 }
 
+/**
+ * Paste Event handler. Opens dialog and call __convertImgToBase64().
+ * @param {object} e The event.
+ */
 Dragio.prototype._pasteCall = function (e) {
+    this.open();
     this._convertImgToBase64(e, bind(this, this._pasteSubmit));
 }
 
+/**
+ * Submit form if the img is valid.
+ * @param {string} img The image.
+ */
 Dragio.prototype._pasteSubmit = function(img) {
     if(typeof img === "undefined") {
         this.displayMessage("Invalid image.");
@@ -140,8 +422,13 @@ Dragio.prototype._pasteSubmit = function(img) {
     }
 }
 
-Dragio.prototype.displayMessage = function(msg) {
-    document.getElementById("dragio__" + this._ID + "__info").innerHTML = msg;
+/**
+ * Displays the message in dialog.
+ * @param {string} msg The message.
+ * @param {boolean} [error=false] If sets to true, the text will be red, otherwise it will be white.
+ */
+Dragio.prototype.displayMessage = function(msg, error = false) {
+    document.getElementById("dragio__" + this._ID + "__info").innerHTML = (error ? "<span style='color: red;'>" : "") + msg + (error ? "</span>" : "");
     document.getElementById("dragio__" + this._ID + "__hint").innerHTML = " ";
     let id = this._ID;
     setTimeout(function() {
@@ -153,6 +440,12 @@ Dragio.prototype.displayMessage = function(msg) {
     }, 5000);
 }
 
+/**
+ * Convert image from clipboard to base64 string.
+ * @param {object} pasteEvent The paste event.
+ * @param {function} callback The callback function.
+ * @param {string} imageFormat The format of image, e.g. image/png. By default, image/png.
+ */
 Dragio.prototype._convertImgToBase64 = function(pasteEvent, callback, imageFormat) {
 
     if (pasteEvent.clipboardData == false) {
@@ -198,6 +491,28 @@ Dragio.prototype._convertImgToBase64 = function(pasteEvent, callback, imageForma
 }
 
 /**
+ * Converts bytes to human readable form.
+ * @param {number} bytes The number of bytes that will be converted.
+ * @param {boolean} [si=false] If sets to false, the function will be divide the bytes by 1024, otherwise by 1000.
+ * @return {string} The converted value with unit, e.g. _convertBytes(1024) will return "1 KB"
+ */
+Dragio.prototype._convertBytes = function(bytes, si = false) {
+    var thresh = si ? 1000 : 1024;
+    if (Math.abs(bytes) < thresh) {
+        return bytes + ' B';
+    }
+    var units = si
+        ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+        : ['KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YE'];
+    var u = -1;
+    do {
+        bytes /= thresh;
+        ++u;
+    } while (Math.abs(bytes) >= thresh && u < units.length - 1);
+    return bytes.toFixed(1) + ' ' + units[u];
+}
+
+/**
  * Initializes Dragio.
  */
 Dragio.prototype._init = function () {
@@ -214,7 +529,7 @@ Dragio.prototype._init = function () {
     var html = 
 `<div class="dragio__overlay" id="dragio__` + this._ID + `__overlay" style="display:none; opacity: 0;">
     <div class="dragio__exit" id="dragio__` + this._ID + `__exit">&times;</div>
-    <div class="dragio__flex-container">
+    <div class="dragio__flex-container" id="dragio__` + this._ID + `__container">
         <div class="dragio__row">
             <div class="dragio__flex-item" id="dragio__` + this._ID + `__icon">
                 <svg class="dragio__box__icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="50px" height="50px" viewBox="0 0 50 50" version="1.1" fill="#000000">
@@ -229,6 +544,15 @@ Dragio.prototype._init = function () {
             <div class="dragio__flex-item" id="dragio__` + this._ID + `__hint">
                 Or press Ctrl + V to upload image from clipboard.
             </div>
+        </div>
+    </div>
+    <div class="dragio__files" id="dragio__`+ this._ID + `__files">
+        <div class="dragio__file">
+            <table role="table">
+                <tbody role="rowgroup" id="dragio__`+ this._ID + `__file__dialog">
+                    
+                </tbody>
+            </table>
         </div>
     </div>
     <form action="`+this._URL+`" method="post" enctype="multipart/form-data" style="display: none;" id="dragio__` + this._ID + `__uploadForm">
--- README.md
+++ README.md
@@ -1,4 +1,125 @@
 # Dragio.js
+> Easy file dialog.
 
-* Knihovna, která slouží pro handlování Drag 'n' drop eventů, má různé možnosti, jako například zobrazení modal boxu při enteru dragu apod.
-* Dokáže zpracovat taky paste (`Ctrl+V`)
+
+**Table of Contents**
+- [Dragio.js demo](#demo)
+- [Installing Dragio.js](#installation)
+- [Using Dragio.js](#usage)
+- [Browser Support](#browser-support)
+- [API](#api)
+ - [Dragio.js options](#dragio-options-)
+- [Author](#with-heart-by)
+- [License](#license)
+- [Documentation](#documentation)
+
+## Demo
+
+[Check out the demo](https://alexkratky.github.io/Dragio.js/example.html) to see it in action (on your mobile or emulate touches on your browser).
+
+<img src="https://imgur.com/V4Ngshu" alt="Dragio.js preview">
+
+<img src="https://imgur.com/6elbl1M" alt="Dragio.js preview">
+
+## Installation
+Download Dragio.js and Dragio.css from [releases](https://github.com/AlexKratky/Dragio.js/releases), include them in `<head>` by entering following code:
+```html
+<script src="Dragio.js"></script>
+<link rel="stylesheet" href="Dragio.css">
+```
+## Usage
+```html
+<html>
+    <head>
+        <link rel="stylesheet" href="Dragio.css">
+        <script src="Dragio.js"></script>
+    </head>
+    <body>
+       	<button onclick="dragio.open()">Upload file</button>
+    </body>
+    <script>
+        var dragio = new Dragio({
+            'ID': "dragio1",
+            "URL": "http://localhost:8000/upload",
+            "pasteURL": "http://localhost:8000/paste",
+            "debug": true,
+        });
+    </script>
+</html>
+```
+
+
+
+## Browser Support
+Tested on:
+- Chrome (Android, desktop)
+- MS Edge (desktop)
+
+## API
+
+### Dragio(options)
+Create a new instance of Dragio
+* `options` (Object) - Options to customize a new instance of Dragio.
+* [`options.ID`] (String) - The ID of instance.
+* [`options.URL`] (String) - The URL where will be sent the AJAX request with files
+* [`options.pasteURL`] (String) - The URL where will be sent the AJAX request with the image from clipboard.
+* [`options.container`] (HTMLElement) - The DOM element that will have touch listeners. Default: `body`.
+* [`options.animationTime`] (Number) - The opening animation time in milliseconds. Default: `300`.
+* [`options.callback`] (Function) - The callback function. If it is set, then submit function will call this function.
+* [`options.debug`] (Boolean) - Sets the application output via a console. Default: `false`.
+* [`options.paste`] (Boolean) - Enable or disable paste. Default: `true` which means enabled paste.
+* [`options.justOverlay`] (Boolean) - If it is sets to false, it will open custom file dialog with more information about upload. Default `false`
+
+```js
+var dragio = new Dragio({
+  'container': document.querySelector('body'),
+  'wrapper': document.getElementById('site-wrapper'),
+  'canvas': document.getElementById('site-canvas'),
+  'button': document.getElementById('logo'),
+  'percent': 0.25,
+  'area': 0.1,
+  'debug': 'false',
+  'closeMenuOnWidth': false,
+  'disableMenuOnWidth': false,
+  'size': 300,
+  'animationTime': 300
+});
+```
+
+### Dragio.open();
+Opens the dialog.
+```js
+dragio.open();
+```
+
+### Dragio.close();
+Closes the dialog.
+```js
+dragio.close();
+```
+
+### Dragio.submit();
+Submits the form.
+```js
+dragio.submit();
+```
+
+### Dragio.displayMessage(msg, error = false);
+Displays message.
+```js
+dragio.displayMessage("Hello world", false);
+```
+
+
+## Documentation
+[Dragio.js documentation](https://tssoft.cz/Dragio.js)
+
+## With :heart: by
+- Alex Krátký
+- E-mail: [info@alexkratky.cz](info@alexkratky.cz) 
+- Web: [https://alexkratky.cz/](https://alexkratky.cz)
+- Web: [https://alexkratky.com/](https://alexkratky.com)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
